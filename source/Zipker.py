5c2649278148cc9986e2782a348a25f416a99a3d
BoxBoy (Hong Joongi) <joongi1978@naver.com>
2019-12-09 20:36:53 +0900

    1.2 무차별 대입 및 진행 상황 추가
    무차별 대입 코드를 모두 구축하고 사용자가 현재 대입중인 문자열을 확인할 수 있도록 화면을 추가함.
    화면을 추가함으로서 속도가 지연되므로 사용자의 선택에 따라 표시 여부를 선택할 수 있도록 개선 필요.

--- source/Zipker.py
+++ source/Zipker.py
@@ -2,35 +2,58 @@
 import zipfile
 import sys
 import shutil
+import ctypes
+import os
+import time
 
-# ZIP 정보 수집 파트
-# 압축파일 이름을 zname으로 받음
-zname = input(
-    "===================================\n\n[?] 비밀번호를 찾을 압축파일의 이름을 입력하세요.\n\n===================================\n>>> ")
-is_zname = zipfile.is_zipfile(zname)  # 파일 유무를 True와 False로 저장함
-zfile = zipfile.ZipFile(zname)  # 추후 사용할 zfile 지정
+# 각종 함수 선언 파트
 
-if is_zname != True:
-    print('[!] 입력하신', zname, '압축파일이 존재하지 않습니다.')  # 파일 미존재
-    sys.exit
 
-# 해제 함수 선언 파트
+def MessageBox(title, text, style):
+    # 메시지박스 함수
+    return ctypes.windll.user32.MessageBoxW(None, text, title, style)
 
 
 def zip_decrypt(password):
-    zfile.extractall(path='temp', pwd=password.encode())
+    zfile.extractall(path='temp', pwd=password.encode())  # 비밀번호 대입 함수
 
+
+# ZIP 정보 수집 파트
+# 압축파일 이름을 zname으로 받음
+
+while True:
+    zname = input(
+        "\n        ===================================\n\n[?] 비밀번호를 찾을 압축파일의 이름을 입력하세요.\n\n        ===================================\n>>> ")
+    is_zname = zipfile.is_zipfile(zname)  # 파일 유무를 True와 False로 저장함
+
+    if is_zname != True:
+        user_error_yn = MessageBox('Zipker - 압축파일 비밀번호 해제',
+                                   '입력하신 Zip 파일을 찾지 못하였습니다.\n[Error Code = F-1]\n\n입력하신 이름이 올바른지 다시 한번 확인하여 주시고\n본 파일과 동일한 디렉토리에 존재하는지 확인하여 주십시오.\n자세한 내용은 동봉된 설명서를 참고하세요.', 5)
+        if user_error_yn <= 2:
+            sys.exit()
+        else:
+            os.system('cls')
+            continue
+    else:
+        break
+
+zfile = zipfile.ZipFile(zname)  # 추후 사용할 zfile 지정
 
 # 무차별 대입 시도 파트
 
-print("\n\n===================================\n\n[!] 지금부터", zname,
-      "에 대한 무차별 대입 공격을 진행하겠습니다.\n[!] 오랫동안 찾지 못할 경우 해결이 불가능한 경우이므로 종료하십시오.\n\n===================================")
+os.system('cls')
+print("\n        ===================================\n\n[!] 지금부터", zname,
+      "에 대한 무차별 대입 공격을 진행하겠습니다.\n[!] 오랫동안 찾지 못할 경우 해결이 불가능한 경우이므로 종료하십시오.\n\n        ===================================")
+time.sleep(0.5)
 
 password_decrypt = 0  # 암호 변수
 decrypt_success = 0  # 성공 여부 변수
 
 while decrypt_success != 1:
     try:
+        os.system('cls')
+        print("\n        ===================================\n\n[*] 지금", zname,
+              "에 대한 무차별 대입 공격을 진행중입니다...\n[*] 암호의 길이에 따라 많은 시간이 소요될 수 있습니다...\n\n지금 대입중인 문자열 :", password_decrypt, "\n\n        ===================================")
         zip_decrypt(str(password_decrypt))
         decrypt_success = 1
     except:
@@ -39,10 +62,11 @@ while decrypt_success != 1:
 
 # 결과 보고 파트
 
+os.system('cls')
 if decrypt_success == 1:
     shutil.rmtree(r"temp")  # 압축 해제한 파일 삭제
-    print("\n\n===================================\n\n[*] 비밀번호를 찾았습니다.")
+    print("\n        ===================================\n\n[*] 비밀번호를 찾았습니다.")
     print("[*] 비밀번호는", password_decrypt, "입니다.")
-    print("[*] 본 프로그램은 압축 해제를 지원하지 않으므로 타 프로그램을 사용하여 해제하십시오.\n\n===================================")
+    print("[*] 본 프로그램은 압축 해제를 지원하지 않으므로 타 프로그램을 사용하여 해제하십시오.\n\n        ===================================")
 else:
     print("[!] 비정상적 접근입니다.")
